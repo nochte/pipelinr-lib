549fe0445b699237c6b34521e5526f044e4f6b1c
Nate <nochte@gmail.com>
2021-08-23 21:13:20 -0500

    log when fail

--- retry/retry.go
+++ retry/retry.go
@@ -1,16 +1,23 @@
 package retry
 
-import "time"
+import (
+	"log"
+	"runtime"
+	"time"
+)
 
 // Do tries something <retrycount> times, with <retrybackoff * num retries> between each attempt
 //  on error. if all retries fail, then it returns the last error
 func Do(fn func() error, retrycount int, retrybackoff time.Duration) error {
 	var er error
 	for i := 1; i <= retrycount; i++ {
+
 		er = fn()
 		if er == nil {
 			return nil
 		}
+		_, file, line, _ := runtime.Caller(1)
+		log.Printf("RETRY FAILURE: (file %v) (line %v): %v\n", file, line, er.Error())
 		time.Sleep(retrybackoff * time.Duration(i))
 	}
 	return er
